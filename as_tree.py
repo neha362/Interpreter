82323bb00e5056d943de13b540ceae642cc7147a
Neha Sudarshan <neha.sudarshan189@gmail.com>
2025-07-31 23:55:22 -0400

    Augment BNF - Began implementation of program, declaration list

--- as_tree.py
+++ as_tree.py
@@ -23,22 +23,47 @@ class AST_Node():
 
 #class Program stores appropriate functions for high-level programs
 class Program(AST_Node):
-    def __init__(self, statements, name, env={}):
+    def __init__(self, name, declarations, statements):
+        self.declarations = declarations
         self.statements = statements
         self.env = env
         self.name = name
     
     def invariant(self):
-        return isinstance(self.statements, CompoundStatement)
+        return isinstance(self.statements, StatementList)and isinstance(self.name, Variable) and isinstance(self.declarations, DeclarationList)
 
     def interpret(self):
-        return self.statements.interpret(self.env)
+        if not self.invariant():
+            raise Exception("invalid program node")
+        self.env = self.declarations.interpret()
+        self.name = self.name.interpret()
+        for i in self.statements.interpret(self.env):
+            if not i:
+                return False
+        return True 
 
     def to_string(self, tabs=0):
         return "PROGRAM (" + self.name + ")\n" + self.statements.to_string(tabs + 1)
 
     def __str__(self):
         return "PROGRAM (" + self.name + ")\n" + self.statements.__str__()
+    
+#class DeclarationList stores appropriate functions for a list of declarations
+class DeclarationList(AST_Node):
+    def __init__(self, declarations):
+        self.declarations = declarations
+
+    def invariant(self):
+        for i in self.declarations:
+            if not isinstance(i, Declaration):
+                return False
+        return True
+    
+    def interpret(self, env):
+        for i in self.declarations:
+            env = i.interpret(env)
+        self.env = env
+        return self.env
     
 #class CompoundStatement stores appropriate functions for compound statements
 class CompoundStatement(AST_Node):
